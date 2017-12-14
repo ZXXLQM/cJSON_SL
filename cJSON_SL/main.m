c133086c21f9a1a79d7b1fbaa45ccbd179ed85b0
SweetLoser <zengxiangxiang@cengxiangxiangdeMacBook-Pro.local>
2017-12-14 13:49:39 +0800

    初始化

---
+++ cJSON_SL/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  cJSON_SL
+//
+//  Created by 曾祥翔 on 2017/12/14.
+//  Copyright © 2017年 SweetLoser. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
